ea8e5aae4ac509cde535592d22fa6494910b5464
Rhythmjain05 <rhythmjain9290@gmail.com>
2024-02-06 11:24:37 +0530

    Added and updated the blog page

--- client/travelapp/src/Blog.js
+++ client/travelapp/src/Blog.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Blogslider from './Blogslider'
+import './Blog.css'
 
 function Blog() {
   return (
-    <div>
-      blog
+    <div className='colors'>
+      <div className="nameh">
+        <div className="heading1">
+          Our Blog
+        </div>
+        <hr className='hrs'></hr>
+        <div className="heading2">
+        An insight the credible experience in the world.
+        </div>
+      </div>
+     <div className="blogslider"><Blogslider></Blogslider></div>
     </div>
   )
 }
